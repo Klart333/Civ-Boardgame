a6663fa8a95224b70441ae56a25a62b0fc3f0667
Klart333 <56590003+Klart333@users.noreply.github.com>
2021-02-10 08:29:26 +0100

    set up pt. 2

---
+++ Assets/Scripts/UILoadScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UILoadScene : MonoBehaviour
+{
+    [SerializeField]
+    private int sceneIndexToLoad = 0;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneIndexToLoad);
+    }
+}
