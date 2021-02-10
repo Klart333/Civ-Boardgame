fa3019d7ba7b82947735bb27ddffa873a92aba16
Klart333 <56590003+Klart333@users.noreply.github.com>
2021-02-10 09:03:35 +0100

    push

---
+++ Assets/Scripts/UIConstrictMapInput.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIConstrictMapInput : MonoBehaviour
+{
+    private string currentAcceptedText = "";
+
+    private TMP_InputField inputField;
+
+    private void Start()
+    {
+        inputField = GetComponent<TMP_InputField>();
+    }
+
+    public void ConstrictInput()
+    {
+        string sizeText = inputField.text;
+        if (sizeText.Length >= currentAcceptedText.Length)
+        {
+            string newText = sizeText.Remove(0, currentAcceptedText.Length);
+
+            int result = 0;
+            if (int.TryParse(newText, out result))
+            {
+                if (int.Parse(sizeText) > 12)
+                {
+                    inputField.text = "12";
+                    currentAcceptedText = "12";
+                }
+                else
+                {
+                    currentAcceptedText = sizeText;
+                }
+            }
+            else
+            {
+                inputField.text = currentAcceptedText;
+            }
+        }
+        else // Otherwise we are deleting 
+        {
+            currentAcceptedText = currentAcceptedText.Remove(currentAcceptedText.Length - 1);
+        }
+        
+    }
+}
