a0254d88dcac58f987b4bef6b46e4935acf193b1
Klart333 <56590003+Klart333@users.noreply.github.com>
2021-02-10 09:23:36 +0100

    push

---
+++ Assets/Scripts/UIGenerateMap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIGenerateMap : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_InputField mapSizeInput;
+
+    public void GenerateMap()
+    {
+        int mapSize = int.Parse(mapSizeInput.text);
+    }
+}
