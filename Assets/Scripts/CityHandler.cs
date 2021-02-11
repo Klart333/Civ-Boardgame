1f2d5885d916a57885dbde4120079da75a213f00
Klart333 <roddis.john1@gmail.com>
2021-02-11 22:06:30 +0100

    yes

---
+++ Assets/Scripts/CityHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CityHandler : MonoBehaviour
+{
+    public const int goldGoldAmount = 100;
+    public const int grassGoldAmount = 40;
+    public const int riverGoldAmount = 75;
+    public const int grassWithWaterGoldAmount = 60;
+    public const int sandGoldAmount = 5;
+    public const int snowGoldAmount = 15;
+    public const int stoneGoldAmount = 50;
+    public const int treeGoldAmount = 50;
+
+    [SerializeField]
+    private TMP_InputField nameInput;
+
+    [SerializeField]
+    private TMP_InputField goldInput;
+
+    [SerializeField]
+    private TMP_InputField grassInput;
+
+    [SerializeField]
+    private TMP_InputField riverInput;
+
+    [SerializeField]
+    private TMP_InputField grassWithWaterInput;
+
+    [SerializeField]
+    private TMP_InputField sandInput;
+
+    [SerializeField]
+    private TMP_InputField snowInput;
+
+    [SerializeField]
+    private TMP_InputField stoneInput;
+
+    [SerializeField]
+    private TMP_InputField treeInput;
+
+    private List<City> savedCities = new List<City>();
+
+    public void AddCity()
+    {
+        var city = new City(nameInput.text, int.Parse(goldInput.text) * goldGoldAmount + int.Parse(grassInput.text) * grassGoldAmount + int.Parse(riverInput.text) * riverGoldAmount + int.Parse(grassWithWaterInput.text) * grassWithWaterGoldAmount + int.Parse(sandInput.text) * sandGoldAmount + int.Parse(snowInput.text) * snowGoldAmount + int.Parse(stoneInput.text) * stoneGoldAmount + int.Parse(treeInput.text) * treeGoldAmount);
+        savedCities.Add(city);
+    }
+}
+
+struct City
+{
+    public string name;
+    public int gold;
+
+    public City(string name, int gold)
+    {
+        this.name = name;
+        this.gold = gold;
+    }
+}
---
+++ Assets/Scripts/DiceHandler.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceHandler : MonoBehaviour
+{
+    public Sprite[] Sides;
+
+    [SerializeField]
+    private GameObject dice;
+
+    [SerializeField]
+    private TextMeshProUGUI spinningText;
+
+    [SerializeField]
+    private TextMeshProUGUI resultText;
+
+    [SerializeField]
+    private Button rollButton;
+
+    private List<SpriteRenderer> die = new List<SpriteRenderer>();
+
+    private int dieResult = 0;
+
+    private Vector2 diceSpawnPosition = new Vector2(0, -3);
+
+    private int maxDice = 7;
+    private int diceCount = 0;
+
+    private void Start()
+    {
+        IncreaseDieAmount();
+    }
+
+    public void IncreaseDieAmount()
+    {
+        if (diceCount >= maxDice)
+        {
+            return;
+        }
+
+        diceCount++;
+
+        if (die.Count % 2 == 0) // Even
+        {
+            var _dice = Instantiate(dice, diceSpawnPosition, Quaternion.identity);
+            die.Add(_dice.GetComponent<SpriteRenderer>());
+            diceSpawnPosition.x += _dice.transform.localScale.x * 1.1f;
+        }
+        else // Odd
+        {
+            die.Add(Instantiate(dice, new Vector2(-diceSpawnPosition.x, diceSpawnPosition.y), Quaternion.identity).GetComponent<SpriteRenderer>());
+        }
+        
+    }
+
+    public void DecreaseDieAmount()
+    {
+        if (diceCount <= 0)
+        {
+            return;
+        }
+
+        diceCount--;
+
+        var diceToRemove = die[die.Count - 1];
+        diceSpawnPosition.x -= die.Count % 2 != 0 ? diceToRemove.transform.localScale.x * 1.1f : 0;
+        Destroy(diceToRemove.gameObject);
+        die.Remove(diceToRemove);
+    }
+
+    public void RollDice()
+    {
+        dieResult = 0;
+
+        StartCoroutine(SecondStageEarInfection());
+    }
+
+    private IEnumerator SecondStageEarInfection()
+    {
+        rollButton.interactable = false;
+        spinningText.gameObject.SetActive(true);
+
+        yield return RollDiceGraphic();
+
+        spinningText.gameObject.SetActive(false);
+        rollButton.interactable = true;
+
+        resultText.text = dieResult.ToString();
+    }
+
+    private IEnumerator RollDiceGraphic()
+    {
+        int dieSpins = 10;
+
+        for (int i = 0; i < dieSpins; i++)
+        {
+            for (int g = 0; g < die.Count; g++)
+            {
+                int randomDieSide = Random.Range(0, 6);
+                die[g].sprite = Sides[randomDieSide];
+
+                if (i == dieSpins - 1)
+                {
+                    dieResult += randomDieSide + 1;
+                }
+            }
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
